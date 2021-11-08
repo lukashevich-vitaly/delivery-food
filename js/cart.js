514f3f38de9311df51ba9e6eaca4df3955cb7884
lukashevich-vitaly <lykashevich.vitalij@gmail.com>
2021-11-08 22:36:20 +0300

    feat: mod cart

--- js/cart.js
+++ js/cart.js
@@ -3,12 +3,35 @@ const cart = () => {
     const modalCart = document.querySelector('.modal-cart')
     const close = modalCart.querySelector('.close')
     const body = modalCart.querySelector('.modal-body')
+    const buttonSend = modalCart.querySelector('.button-primary')
+
+    const resetCart = () => {
+        body.innerHTML = ''
+        localStorage.removeItem('cart')
+        modalCart.classList.remove('is-open')
+    }
 
     const incrementCount = (id) => {
-
+        const cartArray = JSON.parse(localStorage.getItem('cart'))
+        cartArray.map((item) => {
+            if(item.id === id) {
+                item.count++
+            }
+            return item
+        })
+        localStorage.setItem('cart', JSON.stringify(cartArray))
+        renderItems(cartArray)
     }
     const decrementCount = (id) => {
-
+        const cartArray = JSON.parse(localStorage.getItem('cart'))
+        cartArray.map((item) => {
+            if(item.id === id) {
+                item.count = item.count > 0 ? item.count -1 : 0;
+                }
+            return item
+        })
+        localStorage.setItem('cart', JSON.stringify(cartArray))
+        renderItems(cartArray)
     }
     
     const renderItems = (data) => {
@@ -20,23 +43,42 @@ const cart = () => {
                 <span class="food-name">${name}</span>
                 <strong class="food-price">${price} ₽</strong>
                 <div class="food-counter">
-                    <button class="counter-button btn-dec">-</button>
+                    <button class="counter-button btn-dec" data-index="${id}">-</button>
                     <span class="counter">${count}</span>
-                    <button class="counter-button btn-inc">+</button>
+                    <button class="counter-button btn-inc" data-index="${id}">+</button>
                 </div>
             `
-            cartElem.querySelector('.btn-dec').addEventListener('click', () => {
-                decrementCount(id)
-            })
-            cartElem.querySelector('.btn-inc').addEventListener('click', () => {
-                incrementCount(id)
-            }) 
             body.append(cartElem)
         });
     }
 
+    body.addEventListener('click', (event) => {
+        event.preventDefault()
+        if(event.target.classList.contains('btn-inc')) {
+           incrementCount(event.target.dataset.index)
+        } else if(event.target.classList.contains('btn-dec')) {
+            decrementCount(event.target.dataset.index)
+        }
+    })
+
+    buttonSend.addEventListener('click', () => {
+        const cartArray = localStorage.getItem('cart')
+
+        fetch('https://jsonplaceholder.typicode.com/posts', {
+            method: 'POST',
+            body: cartArray
+        })
+        .then(response => {
+            if(response.ok){
+                resetCart()
+            }
+        })
+        .catch(e => {
+            console.error(e)
+        })
+    })
+
     buttonCart.addEventListener('click', () => {
-        console.log(JSON.parse(localStorage.getItem('cart')));
         if(localStorage.getItem('cart')) {
             renderItems(JSON.parse(localStorage.getItem('cart')))
         }
